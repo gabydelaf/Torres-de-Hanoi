025a4f92a8b5f697e25c9861bc5ea4b21ba3f1c1
gabydelaf <90404901+gabydelaf@users.noreply.github.com>
2022-09-30 22:47:32 -0500

    Update Hanoi.asm

--- Hanoi.asm
+++ Hanoi.asm
@@ -1,100 +1,80 @@
 #Integrantes:
 #Gabriela Alejandra de la Fuente Chavez
 #Daniel Gutierrez Bustos
+
 .data
-address: .word 0
+.eqv discos 3
+adress: .word 0
+
+
+
 .text
-addi s1 zero 3#numero de discos
-add a0 zero s1#copia/contador
-addi s2 zero 1#numero para comparar
-
-addi a1 zero 0#apuntador a los discos
-
-addi s3 zero 0#Apuntador a la torre origen
-addi s4 zero 0#Apuntador a la torre aux
-addi s5 zero 0#Apuntador a la torre destino
+addi s1 zero 0 #Apuntador a la torre origen
+addi s2 zero 0 #Apuntador a la torre aux
+addi s3 zero 0 #Apuntador a la torre destino
 
 
-addi t0 zero 0#Registro auxiliar para hacer cambio de torre
 
-#MAIN
-add t1 zero sp#se guarda el valor del sp
-jal ra INIT#se manda llamar a la funcion para inicializar torres
-add sp zero t1#se recupera el valor guardado
-jal ra HANOI#se llama la funcion recursiva
-jal zero END
+addi a1 zero discos #número de elementos
+addi a0 zero discos #contador llenado
+addi s5 zero 1 #1 para comparar
 
 
-INIT:	
-	lui sp %hi(address)#se reserva stack para las tres torres
-	addi sp sp %lo(address)
-	add s3 s3 sp#se carga la direccion de las torres vacias #origen
-	addi sp sp 32#en los apuntadores
-	add s4 s4 sp#aux
-	addi sp sp 32
-	add s5 s5 sp#destino
-	
-	#Se comienza a llenar la torre origen
-FILL:	sw a0 0(s3)#se añade el disco
-	addi a0 a0 -1 #se decrementa el numero de discos
-	addi s3 s3 4 #se aumenta el apuntador
-	addi a3 a3 1#se aumenta el contador
-	blt zero a0 FILL#mientras no se hayan cargado todos los discos vuelve a hacerlo
-	addi s3 s3 -4 #regresa a apuntar al ultimo disco
-	jalr zero ra 0 #retorna a la funcion padre
-	
-	
 
-HANOI: 
-	addi sp sp -16 #se reserva stack
-	sw ra 0(sp)#se almacenan los valores
-	sw s3 4(sp)
-	sw s4 8(sp)#en el stack
-	sw s5 12(sp)
+   #main    
+    jal ra init
+    jal ra hanoi
+    jal zero end
+    
+init:    lui s1 %hi(adress) #crear apuntadores
+    addi s1 s1 %lo(adress)    
+    slli s2 a1 2
+    add s2 s1 s2
+    slli s3 a1 2
+    add s3 s2 s3
+    
+    
+fill:    sw a0 0(s1)#se añade el disco
+    addi a0 a0 -1 #se decrementa el numero de discos
+    addi s1 s1 4 #se aumenta el apuntador
+    addi a3 a3 1 #se aumenta el contador
+    blt zero a0 fill #mientras no se hayan cargado todos los discos vuelve a hacerlo
+    addi s1 s1 -4 #regresa a apuntar al ultimo disco
+    jalr zero ra 0 #retorna al main
+    
+hanoi:     blt a3 s5 loop # si el numero de discos es mayor a 1 pasa al loop recursivo
+    addi sp sp -8 #Se le resta 8 al sp
+    sw ra 0(sp)  #push ra
+    sw a3 4 (sp) #push a3  #siempre problemas aqui
+    jal zero return_hanoi #si es igual a 1 comienza a retornar
+    
+loop:    addi a1 a1 -1 #decrementar el numero de elementos
+    addi a3 a3 -1
+    add t0 zero s2 #primer cambio
+    add s2 zero s3
+    add s3 zero t0
+    
+    jal ra hanoi
+    
+    add t0 zero s2 #desawp
+    add s2 zero s3
+    add s3 zero t0
 
-	
-		
-	blt s2 s1 LOOP # si el numero de discos es mayor a 1 pasa al loop recursivo
-	
-	jal zero RETURN#si es igual a 1 comienza a retornar
-	
-LOOP: 	addi s1 s1 -1
-	add t0 zero s4
-	add s4 zero s5
-	add s5 zero t0
-	jal ra HANOI
-	#se cambian los discos
-	
-LOOP2:	lw a1 0(s3)
-	sw zero 0(s3)
-	
-	addi s3 s3 -4
-	sw a1 0(s5)
-	sw s3 4(sp)
-	
-	addi s1 s1 -1
-	add t0 zero s3
-	add s3 zero s5
-	add s5 zero t0
-	
-	jal ra HANOI
-	lw ra 0(sp)
-	lw s3 4(sp)
-	lw s4 8(sp)
-	lw s5 12(sp)
-	
-	lw a1 0(s3)
-	sw zero 0(s3)
-	addi s3 s3 -4
-	sw a1 0(s5)
-	sw s3 4(sp)
-	
-RETURN:	lw ra 0(sp)
-	lw s3 4(sp)
-	lw s4 8(sp)
-	lw s5 12(sp)
-	addi sp sp 16
-	
-	jalr zero ra 0 #retorna a la funcion padre
-END:	
 
+
+   add t0 zero s1 #segundo cambio
+    add s1 zero s3
+    add s3 zero t0
+    
+    jal ra hanoi
+    
+    add t0 zero s1 #segundo deswap
+    add s1 zero s3
+    add s3 zero t0
+            
+return_hanoi:
+    lw ra  0(sp) #Pop primero se lee y luego se incrementa
+    lw a3 4 (sp) #pop a3
+    addi sp sp 8
+    jalr zero ra 0
+end:
